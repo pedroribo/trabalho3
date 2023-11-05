b35f83bcff0f85dfec22322e118641a586ba2ca5
Pedro <pedro@Debian2.myguest.virtualbox.org>
2023-11-04 23:32:59 -0300

    Primeiro commit para salvar arquivos iniciais e primeira versão do código.

---
+++ trabalho3.py
@@ -0,0 +1,37 @@
+import hashlib
+
+# Definindo a senha para a comparacao
+definedPassword_file = open("definedPassword.txt", "w")
+password = "strongPassword"
+hashing_definedPassword = hashlib.sha256()
+hashing_definedPassword.update(password.encode('utf-8'))
+definedPassword_hash = hashing_definedPassword.hexdigest()
+definedPassword_file.write(definedPassword_hash)
+definedPassword_file.close()
+
+definedPassword_file = open("definedPassword.txt", "r")
+definedPassword = definedPassword_file.read()
+definedPassword_file.close()
+print(definedPassword)
+
+# Definindo a senha informada para ser comparada com a senha definida
+informedPassword_file = open("informedPassword.txt", "w")
+informedPassword_file.write("blablabla")
+informedPassword_file.close()
+
+informedPassword_file = open("informedPassword.txt", "r")
+informedPassword = informedPassword_file.read()
+informedPassword_file.close()
+print(informedPassword)
+
+# Gerando hash da senha informada
+hashing_informedPassword = hashlib.sha256()
+hashing_informedPassword.update(informedPassword.encode('utf-8'))
+informedPassword_hash = hashing_informedPassword.hexdigest()
+print(informedPassword_hash)
+
+# Comparando senhas
+if informedPassword_hash == definedPassword:
+    print("Senha correta.")
+else:
+    print("Senha incorreta.")
